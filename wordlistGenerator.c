5e76a42a4311ddc00ccfa26b64c3137fdafcc09b
Richard <richard760x@hotmail.com>
2021-05-24 07:49:47 -0400

    updated the manual

--- wordlistGenerator.c
+++ wordlistGenerator.c
@@ -44,9 +44,9 @@ void man() {
   printf("\t-max MAX \n\t\t set maximum word length to MAX (MAX must be "
          "greater or equal 1)\n\n");
   printf("\tNote: \n\t\t to set the word length to exactly n");
-  printf(", use: ./wordlistGenerator -min n -max n\n");
-  printf("\t\t to set the word length from n to m (n must be greater or equal "
-         "m),\n");
+  printf(", use: ./wordlistGenerator -o OUT -min n -max n\n");
+  printf("\t\t to set the word length from n to m (m must be greater or equal "
+         "n),\n");
   printf("\t\t use: ./wordlistGenerator -o OUT -min n -max m\n\n");
 
   printf("semi-required argument(s) (choose at least one):\n\n");
@@ -64,8 +64,8 @@ void man() {
 
   printf("\t-a APPEND \n\t\t append APPEND to the alphabet");
   printf(", note you need to escape special characters\n");
-  printf("\t\t Use: ./wordlistGenerator -o OUT -min MIN -max MAX -c "
-         "\"\\!@#$%%\"\n\n");
+  printf(
+      "\t\t Use: ./wordlistGenerator -o OUT -min MIN -max MAX -a APPEND\n\n");
 }
 
 int main(int argc, char *argv[]) {
